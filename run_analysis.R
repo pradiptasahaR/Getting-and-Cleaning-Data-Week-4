25cfcbd329b49efd6766cc958d537e21965b4973
pradiptasahaR <pradipta.saha@gmail.com>
2016-03-20 18:17:00 -0400

    updated with comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+# read features, activity labels, test and training data
 features <- read.table("features.txt")
 activities <- read.table("activity_labels.txt")
 
@@ -9,15 +10,26 @@ xtest <- read.table("./test/X_test.txt")
 ytest <- read.table("./test/y_test.txt")
 subjectstest <- read.table("./test/subject_test.txt")
 
+#combine test and training data and assign column names to each of the components of the features vector
 xall <- rbind(xtrain,xtest)
 yall <- rbind(ytrain,ytest)
 subjectsall <- rbind(subjectstrain,subjectstest)
 colnames(xall)<- features[,2]
 
+#retain only those columns that explicitly contain "mean()" or "std()"
 xallsub <- xall[,grepl("mean()",features[,2],fixed=TRUE) | grepl("std()",features[,2],fixed=TRUE)]
+#include columns to describe the subject and activity for each record
 xallsub2 <- cbind(subjectsall,yall,xallsub)
+
+#replace activity number identifiers by activity labels
 for (i in 1:nrow(xallsub2)) xallsub2[i,2] <- as.character(activities[xallsub2[i,2],2])
+
+#give descriptive names to first two columns
 colnames(xallsub2)[1:2] <- c("subject","activity")
+
+#compute averages by subject and activity
 bysubjectactivity <- group_by(xallsub2,subject,activity)
 finaltidy <- summarize_each(bysubjectactivity,funs(mean))
+
+# create output text file
 write.table(finaltidy,file="result.txt",row.name=FALSE)
